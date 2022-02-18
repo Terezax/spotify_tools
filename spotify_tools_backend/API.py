e0f661ee60e37ba68b07c0954d2c538720a09b83
Tereza Stysova <tereza.stysova@gmail.com>
2022-02-18 21:41:48 +0100

    uvozovky

--- spotify_tools_backend/API.py
+++ spotify_tools_backend/API.py
@@ -76,10 +76,10 @@ def get_spotify_oauth_token():
 @app.route('/userProfile')
 def getUserProfile():
     resp = Response("""{
-        id: "A",
-        displayName: "Franta Vykoukal",
-        email: "franta@volny.cz",
-        imageUrl: ""
+        "id": "A",
+        "displayName": "Franta Vykoukal",
+        "email": "franta@volny.cz",
+        "imageUrl": ""
     }""", mimetype='application/json')
     return resp
 
@@ -87,16 +87,16 @@ def getUserProfile():
 def getPlaylists():
     resp = Response("""[
         {
-            id: "A",
-            name: "Playlist A",
-            url: "https://www.seznam.cz",
-            numberOfTracks: 3
+            "id": "A",
+            "name": "Playlist A",
+            "url": "https://www.seznam.cz",
+            "numberOfTracks": 3
         },
         {
-            id: "B",
-            name: "Playlist B",
-            url: "https://www.seznam.cz",
-            numberOfTracks: 4
+            "id": "B",
+            "name": "Playlist B",
+            "url": "https://www.seznam.cz",
+            "numberOfTracks": 4
         }
     ]""", mimetype='application/json')
     return resp
@@ -104,53 +104,53 @@ def getPlaylists():
 @app.route('/playlists/$compare')
 def getComparePlaylists():
     resp = Response("""{
-        tracksMissingFromPlaylist1: [
+        "tracksMissingFromPlaylist1": [
             {
-                id: "A",
-                name: "Smells Like Teen Spirit",
-                url: "https://open.spotify.com/track/5ghIJDpPoe3CfHMGu71E6T",
-                duration: 143000,
-                albumName: "Nevermind",
-                albumUrl: "https://open.spotify.com/album/2UJcKiJxNryhL050F5Z1Fk?highlight=spotify:track:4CeeEOM32jQcH3eN9Q2dGj",
-                albumImageUrl: "https://i.scdn.co/image/ab67616d00001e02e175a19e530c898d167d39bf",
-                artists: [{
-                    id: "A",
-                    name: "Nirvana",
-                    url: "https://open.spotify.com/artist/6olE6TJLqED3rqDCT0FyPh"
+                "id": "A",
+                "name": "Smells Like Teen Spirit",
+                "url": "https://open.spotify.com/track/5ghIJDpPoe3CfHMGu71E6T",
+                "duration": 143000,
+                "albumName": "Nevermind",
+                "albumUrl": "https://open.spotify.com/album/2UJcKiJxNryhL050F5Z1Fk?highlight=spotify:track:4CeeEOM32jQcH3eN9Q2dGj",
+                "albumImageUrl": "https://i.scdn.co/image/ab67616d00001e02e175a19e530c898d167d39bf",
+                "artists": [{
+                    "id": "A",
+                    "name": "Nirvana",
+                    "url": "https://open.spotify.com/artist/6olE6TJLqED3rqDCT0FyPh"
                 }]
             }
         ],
-        tracksMissingFromPlaylist2: [
+        "tracksMissingFromPlaylist2": [
             {
-                id: "B",
-                name: "Song B",
-                url: "https://www.seznam.cz",
-                duration: 181000,
-                albumName: "Album B",
-                albumUrl: "https://www.seznam.cz",
-                albumImageUrl: "",
-                artists: [{
-                    id: "B",
-                    name: "Artist B",
-                    url: "https://www.seznam.cz"
+                "id": "B",
+                "name": "Song B",
+                "url": "https://www.seznam.cz",
+                "duration": 181000,
+                "albumName": "Album B",
+                "albumUrl": "https://www.seznam.cz",
+                "albumImageUrl": "",
+                "artists": [{
+                    "id": "B",
+                    "name": "Artist B",
+                    "url": "https://www.seznam.cz"
                 }]
             },
             {
-                id: "C",
-                name: "Song C",
-                url: "https://www.seznam.cz",
-                duration: 210000,
-                albumName: "Album C",
-                albumUrl: "https://www.seznam.cz",
-                albumImageUrl: "",
-                artists: [{
-                    id: "C",
-                    name: "Artist C",
-                    url: "https://www.seznam.cz"
+                "id": "C",
+                "name": "Song C",
+                "url": "https://www.seznam.cz",
+                "duration": 210000,
+                "albumName": "Album C",
+                "albumUrl": "https://www.seznam.cz",
+                "albumImageUrl": "",
+                "artists": [{
+                    "id": "C",
+                    "name": "Artist C",
+                    "url": "https://www.seznam.cz"
                 }, {
-                    id: "D",
-                    name: "Artist D",
-                    url: "https://www.seznam.cz"
+                    "id": "D",
+                    "name": "Artist D",
+                    "url": "https://www.seznam.cz"
                 }]
             }
         ]
